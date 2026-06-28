a6fea8bb3b4e3b767e3c9fad249e696ef2f596a9
George <bluebirdwaterfowl@gmail.com>
2026-06-27 20:57:32 -0700

    feat: guard remaining product + package-order mutation endpoints with can_edit (view-only is now fully read-only)

--- ajax/orders/package_order_cancel.php
+++ ajax/orders/package_order_cancel.php
@@ -2,6 +2,8 @@
 
 	require_once(__DIR__."/../../includes/fns.php");
 	require_login();
+	require_can(can_edit('orders'), 'You do not have permission to perform this action.');
+	require_login();
 
 	$db = db_connect();
 	$id = (int)$_POST['id'];
--- ajax/orders/package_order_send.php
+++ ajax/orders/package_order_send.php
@@ -1,6 +1,8 @@
 <?php
 
 	require_once(__DIR__."/../../includes/fns.php");
+	require_login();
+	require_can(can_edit('orders'), 'You do not have permission to perform this action.');
 
 	$dbLink = $mysqli = db_connect();
 
